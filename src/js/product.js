638778a2a4b2e57fedee1e53d971c9fc65d6e48d
Farrux <Farrux@gmail.com>
2025-03-06 20:22:30 +0500

    refactor product display logic to use template for dynamic rendering

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,16 @@
 import { fetchData } from "./fetchData.js";
 import "./dark-mode.js";
 import "./loader.js";
-const card = document.querySelector(".card");
-const cardTitle = document.querySelector(".card-title");
-const cardText = document.querySelector(".text");
-const Prating = document.querySelector(".rating");
-const Preview = document.querySelector(".review");
-const img = document.querySelector(".card-img");
-const priceOfProduct = document.querySelector(".priceOfProduct");
-const discountPrice = document.querySelector(".discountPrice");
+const template = document.querySelector("template");
+const container = document.querySelector(".hero__container");
+// const card = document.querySelector(".card");
+// const cardTitle = document.querySelector(".card-title");
+// const cardText = document.querySelector(".text");
+// const Prating = document.querySelector(".rating");
+// const Preview = document.querySelector(".review");
+// const img = document.querySelector(".card-img");
+// const priceOfProduct = document.querySelector(".priceOfProduct");
+// const discountPrice = document.querySelector(".discountPrice");
 
 const queryString = window.location.search;
 const id = new URLSearchParams(queryString).get("id");
@@ -25,23 +27,25 @@ const showProduct = (product) => {
     reviews,
   } = product;
 
+  const getClone = template.content.cloneNode(true);
+
+  const card = getClone.querySelector(".productCard");
+  const img = getClone.querySelector(".card-img");
+  const cardTitle = getClone.querySelector(".card-title");
+  const cardText = getClone.querySelector(".text");
+  const productRating = getClone.querySelector(".rating");
+  const priceOfProduct = getClone.querySelector(".priceOfProduct");
+  const discountPrice = getClone.querySelector(".discountPrice");
+  const productReview = getClone.querySelector(".review");
+
   img.src = thumbnail;
   cardTitle.textContent = title;
   cardText.textContent = description;
-  Prating.textContent = rating;
+  productRating.textContent = rating;
   priceOfProduct.textContent = `${price}$`;
   discountPrice.textContent = `${(price - (price / 100) * discountPercentage).toFixed(2)}`;
-  Preview.textContent = `${reviews.length} Reviews`;
-
-  //   const getClone = template.content.cloneNode(true);
-  // const card = getClone.querySelector(".card");
-  // const img = getClone.querySelector(".card-img");
-  // const cardTitle = getClone.querySelector(".card-title");
-  // const cardText = getClone.querySelector(".text");
-  // const productRating = getClone.querySelector(".rating");
-  // const priceOfProduct = getClone.querySelector(".priceOfProduct");
-  // const discountPrice = getClone.querySelector(".discountPrice");
-  // const productReview = getClone.querySelector(".review");
+  productReview.textContent = `${reviews.length} Reviews`;
+  container.appendChild(card);
 };
 
 fetchData("https://dummyjson.com/product/" + id)
